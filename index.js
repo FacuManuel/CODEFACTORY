fed5ef7a51fb6664e5e23cc7adf5e051afd1c98b
Facundo Manuel Perez Duco <perezfacundo11@hotmail.com>
2023-09-22 20:54:08 +0200

    portfolio-main

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 
 
+// Función para mostrar un saludo inicial.
+function mostrarSaludo() {
+    Swal.fire({
+      title: 'CodeFactory.',
+      text: '¡Bienvenido a Mi Portfolio!',
+      imageUrl: './images/profile.bmp',
+      imageWidth: 300,
+      imageHeight: 300,
+      imageAlt: 'Imagen de Nike',
+      customClass: {
+  
+        popup: 'custom-swal-popup', 
+      },
+      background: ` url('warning.jpg')`, 
+      backdrop: `
+        rgba(0,0,0)
+        url('images/warning.jpg')
+        top
+        no-repeat
+      `,
+    });
+  }
 
+  mostrarSaludo();
 
 
